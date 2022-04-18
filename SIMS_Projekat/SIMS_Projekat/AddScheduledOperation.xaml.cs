7dd22cdf13355c18b7876ec9914ea5ecd25bd98c
Tatjana Gemovic <gemovic.ra105.2019@uns.ac.rs>
2022-04-18 11:08:38 +0200

    Change to Appointment

--- SIMS_Projekat/SIMS_Projekat/AddScheduledOperation.xaml.cs
+++ SIMS_Projekat/SIMS_Projekat/AddScheduledOperation.xaml.cs
@@ -26,21 +26,80 @@ namespace SIMS_Projekat
         }
         private void DataWindow_Closing(object sender, EventArgs e)
         {
-            App.scheduledOperationRepository.Serialize();
+            App.appointmentRepo.Serialize();
         }
 
         private void Dodaj_operaciju_Click(object sender, RoutedEventArgs e)
         {
-            ScheduledOperation s = new ScheduledOperation();
+            Doctor doctor = new Doctor()
+            {
+                FirstName = "Joka",
+                LastName = "Jokic",
+                Email = "jok@gmail.com",
+                Jmbg = "111122440",
+                Username = "pera",
+                Password = "pera123",
+                PhoneNumber = "0641111111",
+                DateOfBirth = new DateTime(1994, 5, 15),
+                ID = "11",
+                LicenceNumber = "1542014"
+            };
+            Patient patient1 = new Patient()
+            {
+                ID = "210",
+                FirstName = "Ana",
+                LastName = "Anic",
+                Email = "ana@gmail.com",
+                Jmbg = "515120",
+                Username = "ana",
+                Password = "ana123",
+                PhoneNumber = "0645554442",
+                DateOfBirth = new DateTime(2000, 10, 15),
+                BloodType = BloodType.A_Positive,
+                Height = 178.0,
+                Weight = 80.0,
+                HealthInsuranceID = "0426"
+            };
+
+            Room room = new Room()
+            {
+                RoomID = "13",
+                Floor = 4,
+                Type = RoomType.examRoom,
+                RoomNumber = 13,
+                Available = false,
+            };
+            bool op;
+            if (Tip_operacije.Text.Equals("False"))
+            {
+                op = false;
+            }
+            else
+            {
+                op = true;
+            }
+
+            Appointment appointment = new Appointment()
+            {
+                appointmentID = Int32.Parse(ID_operacije.Text),
+                beginningDate = DateTime.Parse(Vreme_pocetka.Text),
+                endDate = DateTime.Parse(Vreme_zavrsetka.Text),
+                operation = op,
+                room = room,
+                doctor = doctor,
+                patient = patient1
+            };
+
+
+            /*ScheduledOperation s = new ScheduledOperation();
             s.Start = DateTime.Parse(Vreme_pocetka.Text);
             s.End = DateTime.Parse(Vreme_zavrsetka.Text);
             s.OperationType = Tip_operacije.Text;
             s.OperationID = int.Parse(ID_operacije.Text);
-            App.ScheduledOperationController.ScheduleOperation(s);
-            App.scheduledOperationRepository.Serialize();
+            App.ScheduledOperationController.ScheduleOperation(s);*/
+            App.appointmentController.AddAppointment(appointment);
             this.Close();
-            //MainWindow main = new MainWindow();
-            //main.Show();
+           
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
