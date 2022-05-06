7046d34af91571b385df6d38dc80a9157966722d
Nemanja Andric <nandric00@gmail.com>
2022-05-06 02:59:32 +0200

    Create manager log out

--- SIMS_Projekat/SIMS_Projekat/ManagerView/ManagerHome.xaml.cs
+++ SIMS_Projekat/SIMS_Projekat/ManagerView/ManagerHome.xaml.cs
@@ -88,5 +88,12 @@ namespace SIMS_Projekat
         {
             MainFrame.Content = new EquipmentView();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
     }
 }
