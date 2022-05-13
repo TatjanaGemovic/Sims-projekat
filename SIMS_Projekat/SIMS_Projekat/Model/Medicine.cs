6a165aaa51417c7fa66b5c38df276adf1a81e690
Mihaela Osmajić <elaosmajic@gmail.com>
2022-05-14 01:10:28 +0200

    #Manager - Medicine model and repository

---
+++ SIMS_Projekat/SIMS_Projekat/Model/Medicine.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS_Projekat.Model
+{
+    public class Medicine : Serialization.Serializable, INotifyPropertyChanged
+    {
+        private string medicineID;
+        private string medicineName;
+        private int medicineDose;
+        private MedicineType medicineType;
+        private MedicineUseType medicineUseType;
+        private Boolean verify;
+        private Boolean sendToDoctor;
+        private Boolean onObservation;
+        private string doctorComment;
+        private List<Medicine> replacmentMedicine;
+        private List<String> medicineComponents;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Medicine()
+        {
+            replacmentMedicine = new List<Medicine>();
+            medicineComponents = new List<string>();
+        }
+
+        public void fromCSV(string[] values)
+        {
+            medicineID = values[0];
+            medicineName = values[1];
+            medicineDose = int.Parse(values[2]);
+            medicineType = (MedicineType)int.Parse(values[3]);
+            medicineUseType = (MedicineUseType)int.Parse(values[4]);
+            if (values[5].Equals("True")) { verify = true; } else { verify = false; }
+            if (values[6].Equals("True")) { sendToDoctor = true; } else { sendToDoctor = false; }
+            if (values[7].Equals("True")) { onObservation = true; } else { onObservation  = false; }
+            doctorComment = values[8];
+        }
+
+        public string[] toCSV()
+        {
+            string[] values = {
+                medicineID,
+                medicineName,
+                medicineDose.ToString(),
+                ((int)medicineType).ToString(),
+                ((int)medicineUseType).ToString(),
+                verify.ToString(),
+                sendToDoctor.ToString(),
+                onObservation.ToString(),
+                doctorComment
+            };
+
+            return values;
+        }
+
+        public string MedicineID
+        {
+
+            get { return medicineID; }
+            set
+            {
+                medicineID = value;
+                OnPropertyChanged(nameof(MedicineID));
+            }
+        }
+
+        public string MedicineName
+        {
+
+            get { return medicineName; }
+            set
+            {
+                medicineName = value;
+                OnPropertyChanged(nameof(MedicineName));
+            }
+        }
+        public MedicineType pMedicineType
+        {
+
+            get { return medicineType; }
+            set
+            {
+                medicineType = value;
+                OnPropertyChanged(nameof(pMedicineType));
+            }
+        }
+
+        public MedicineUseType pMedicineUseType
+        {
+
+            get { return medicineUseType; }
+            set
+            {
+                medicineUseType = value;
+                OnPropertyChanged(nameof(pMedicineUseType));
+            }
+        }
+
+        public int MedicineDose
+        {
+
+            get { return medicineDose; }
+            set
+            {
+                medicineDose = value;
+                OnPropertyChanged(nameof(MedicineDose));
+            }
+        }
+
+        public Boolean Verify
+        {
+            get { return verify; }
+            set
+            {
+                verify = value;
+                OnPropertyChanged(nameof(Verify));
+            }
+        }
+
+        public Boolean SendToDoctor
+        {
+            get { return sendToDoctor; }
+            set
+            {
+                sendToDoctor = value;
+                OnPropertyChanged(nameof(SendToDoctor));
+            }
+        }
+
+        public Boolean OnObservation
+        {
+            get { return onObservation; }
+            set
+            {
+                onObservation = value;
+                OnPropertyChanged(nameof(OnObservation));
+            }
+        }
+
+        public string DoctorComment
+        {
+
+            get { return doctorComment; }
+            set
+            {
+                doctorComment = value;
+                OnPropertyChanged(nameof(DoctorComment));
+            }
+        }
+
+        public List<Medicine> ReplacmentMedicine
+        {
+
+            get { return replacmentMedicine; }
+            set
+            {
+                replacmentMedicine = value;
+                OnPropertyChanged(nameof(ReplacmentMedicine));
+            }
+        }
+
+        public List<string> MedicineComponents
+        {
+
+            get { return medicineComponents; }
+            set
+            {
+                medicineComponents = value;
+                OnPropertyChanged(nameof(MedicineComponents));
+            }
+        }
+
+        public void OnPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+    }
+}
---
+++ SIMS_Projekat/SIMS_Projekat/Repository/MedicineRepository.cs
@@ -0,0 +1,151 @@
+﻿using SIMS_Projekat.Model;
+using SIMS_Projekat.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS_Projekat.Repository
+{
+    public class MedicineRepository
+    {
+        public static List<Medicine> medicine;
+        private Serializer<Medicine> serializer;
+        private string file;
+
+        public List<Medicine> GetMedicine()
+        {
+
+            return medicine;
+        }
+
+        public Medicine GetMedicineByID(string medicineID)
+        {
+            foreach (Medicine _medicine in medicine)
+            {
+                if (_medicine.MedicineID.Equals(medicineID))
+                {
+                    return _medicine;
+                }
+            }
+
+            return null;
+        }
+
+        public List<Medicine> GetVerifyMedicine(string medicineID)
+        {
+            var medicineList = new List<Medicine>();
+            foreach (Medicine _medicine in medicine)
+            {
+                if (_medicine.Verify == true)
+                {
+                    medicineList.Add(_medicine);
+                }
+            }
+
+            return medicineList;
+        }
+
+        public List<Medicine> GetRejectMedicine(string medicineID)
+        {
+            var medicineList = new List<Medicine>();
+            foreach (Medicine _medicine in medicine)
+            {
+                if (_medicine.Verify == false && _medicine.SendToDoctor == true && _medicine.OnObservation == false)
+                {
+                    medicineList.Add(_medicine);
+                }
+            }
+
+            return medicineList;
+        }
+
+        public List<Medicine> GetSendToDoctorMedicine()
+        {
+            var medicineList = new List<Medicine>();
+            foreach (Medicine _medicine in medicine)
+            {
+                if (_medicine.SendToDoctor == true && _medicine.OnObservation == true)
+                {
+                    medicineList.Add(_medicine);
+                }
+            }
+
+            return medicineList;
+        }
+
+        public Medicine DeleteMedicine(Medicine _medicine)
+        {
+            medicine.Remove(_medicine);
+            Serialize();
+            return _medicine;
+        }
+
+        public Medicine EditMedicine(Medicine oldMedicine, Medicine newMedicine)
+        {
+            oldMedicine.MedicineName = newMedicine.MedicineName;
+            oldMedicine.MedicineDose = newMedicine.MedicineDose;
+            oldMedicine.pMedicineType = newMedicine.pMedicineType;
+            oldMedicine.Verify = newMedicine.Verify;
+            oldMedicine.SendToDoctor= newMedicine.SendToDoctor;
+            oldMedicine.OnObservation= newMedicine.OnObservation;
+            oldMedicine.pMedicineUseType = newMedicine.pMedicineUseType;
+            oldMedicine.DoctorComment = newMedicine.DoctorComment;
+            Serialize();
+            return newMedicine;
+
+        }
+
+        public Model.Medicine AddMedicine(Medicine newMedicine)
+        {
+            medicine.Add(newMedicine);
+            Serialize();
+            return newMedicine;
+        }
+
+
+        public List<Medicine> Medicine
+        {
+            get
+            {
+                if (medicine == null)
+                    medicine = new List<Medicine>();
+                return medicine;
+            }
+            set
+            {
+                RemoveAllMedicine();
+                if (value != null)
+                {
+                    foreach (Medicine oMedicine in value)
+                        AddMedicine(oMedicine);
+                }
+            }
+        }
+
+        public void RemoveAllMedicine()
+        {
+            if (medicine != null)
+                medicine.Clear();
+        }
+
+        public MedicineRepository(string fileName)
+        {
+            medicine = new List<Medicine>();
+            serializer = new Serializer<Medicine>();
+            file = fileName;
+
+        }
+
+        public void Serialize()
+        {
+            serializer.toCSV(file, medicine);
+        }
+
+        public void Deserialize()
+        {
+            medicine = serializer.fromCSV(file);
+        }
+    }
+}
