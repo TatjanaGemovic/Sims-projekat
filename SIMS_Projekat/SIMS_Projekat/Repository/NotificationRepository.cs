deccee3f5d5851ef616b722bf31d5c6c31a35bad
Nemanja Andric <nandric00@gmail.com>
2022-06-05 15:12:20 +0200

    Create Notification controller, service, repo, and model

---
+++ SIMS_Projekat/SIMS_Projekat/Repository/NotificationRepository.cs
@@ -0,0 +1,87 @@
+using SIMS_Projekat.Serialization;
+using SIMS_Projekat.Model;
+using System;
+using System.Collections.Generic;
+
+namespace SIMS_Projekat.Repository
+{
+    public class NotificationRepository
+    {
+        public List<Notification> Notifications { get; set; }
+  
+
+        private Serializer<Notification> notificationSerializer;
+
+        private string notificationFile;
+
+        private int ID;
+
+        public NotificationRepository(string notificationFileName)
+        {
+            Notifications = new List<Notification>();
+            notificationSerializer = new Serializer<Notification>();
+            notificationFile = notificationFileName;
+            ID = 100;
+        }
+
+        public void Serialize()
+        {
+            notificationSerializer.toCSV(notificationFile, Notifications);
+        }
+
+        public void Deserialize()
+        {
+            Notifications = notificationSerializer.fromCSV(notificationFile);
+
+            int maxID = 100;
+            foreach(Notification notification in Notifications)
+            {
+                if (int.Parse(notification.ID) > maxID)
+                    maxID = int.Parse(notification.ID);
+            }
+            ID = ++maxID;
+        }
+        public Notification AddNotification(Notification notification)
+        {
+            notification.ID = ID++.ToString();
+            Notifications.Add(notification);
+            return notification;
+        }
+
+        public Notification DeleteNotification(Notification notification)
+        {
+            return Notifications.Remove(notification) ? notification : null;
+        }
+
+        public Notification EditNotification(Notification notification, string notificationID)
+        {
+            foreach (Notification oldNotification in Notifications)
+            {
+                if (oldNotification.ID.Equals(notificationID))
+                {
+                    oldNotification.Topic = notification.Topic;
+                    oldNotification.StartDateTime = notification.StartDateTime;
+                    oldNotification.EndDateTime = notification.EndDateTime;
+                    oldNotification.Description = notification.Description;
+                    oldNotification.InvitedStaff = notification.InvitedStaff;
+                }
+            }
+            return null;
+        }
+
+        public List<Notification> GetAllNotifications()
+        {
+            return Notifications;
+        }
+
+        public Notification GetNotificationByID(string notificationID)
+        {
+            foreach (Notification notification in Notifications)
+            {
+                if (notification.ID.Equals(notificationID))
+                    return notification;
+            }
+            return null;
+        }
+    }
+}
